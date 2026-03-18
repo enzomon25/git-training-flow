392cf3beb5e17b84eb65529281c13dd623ad20a2
Enzo Olórtegui <malstron4@gmail.com>
2026-03-18 01:43:30 -0500

    [demo] more code

--- sum.js
+++ sum.js
@@ -1,1 +1,13 @@
 const fnSum = (a, b) => a + b;
+
+const applyOperation = (operation) => (a,b) => operation(a,b);
+
+const sumarDosNumeros = applyOperation(fnSum);
+
+console.log(sumarDosNumeros(3, 3));
+
+const fnMultiply = (a, b) => a * b;
+
+const multiplicarDosNumeros = applyOperation(fnMultiply);
+
+console.log(multiplicarDosNumeros(4, 2));
