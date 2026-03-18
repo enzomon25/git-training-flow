579f70af87031920f80b8eb12e62fd3c28ecdffc
Enzo Olórtegui <malstron4@gmail.com>
2026-03-18 02:15:33 -0500

    [rebase] cambio dev

--- app.js
+++ app.js
@@ -14,3 +14,5 @@ console.log(sumarDosNumeros(3, 3));
 console.log(multiplicarDosNumeros(4, 2));
 console.log(restarDosNumeros(10, 5));
 console.log(divisionDosNumeros(10, 5));
+
+console.log(applyOperation(sumar)(3, 3));
